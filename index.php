102577e2a7c9d05a647e5185be20e3f6f2ec533c
Nathan Onn <nathan@smashingadvantage.com>
2023-01-03 17:33:01 +0800

    initial setup

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+    // Only change code below this line
+
+        // Instruction: Parse the URL path
+
+
+        // Instruction: Remove the leading and trailing slashes from the path
+        
+
+        // Instruction:  Load the appropriate route content
+            require 'pages/home.php';
+
+    // Only change code above this line
